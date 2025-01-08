03a019d4ff6a93acbbac73441d1a1cfd41807728
adamhuston <adam.huston@bsu.edu>
2025-01-08 15:33:56 -0500

    pig latin not passing tests

---
+++ kata8.py
@@ -0,0 +1,26 @@
+def pig_it(text):
+    print(text)
+    result=""
+    for word in text.split():
+        
+        
+        if word.isalpha():
+            if word[-2:] == "ay":
+                result = result + " " + word
+            else: 
+                word = word[1:] + word[0] + 'ay'
+                result = result + " " + word
+        else:
+            fixable = ""
+            fix = list(word)
+            for i in range(len(fix)):
+                if fix[i].isalpha():
+                    fixable = fixable + fix[i]
+                else:
+                    holdit = fix[i]
+            fixable = fixable[1:] + fixable[0] + 'ay' + holdit
+            result = result + " " + fixable
+
+    return result
+
+print(pig_it('igPay atinlay siay oolcay'))
