d2ca5c9b5480d4ae52336e1a2ebc645a1c7476d4
adamhuston <adam.huston@bsu.edu>
2025-01-17 12:12:55 -0500

    yeesh

--- livedemo3,8and3,9and3,15.py
+++ livedemo3,8and3,9and3,15.py
@@ -4,10 +4,23 @@
 
 # ask user to enter a sentence
 
-phrase = input('Please enter a phrase: ')
+# phrase = input('Please enter a phrase: ')
 
 # print each word on a separate line
 # phrase is what we are looping through
 # eachLetter will have a letter one at a time, as we loop
-for letter in phrase:
-    print(letter)
+#for letter in phrase:
+  #  print(letter)
+#  break
+
+print("AND:")
+print(True and True)
+print(True and False)
+print(False and False)
+print(False and True)
+print('')
+print("OR:")
+print(True or True)
+print(True or False)
+print(False or False)
+print(False or True)
